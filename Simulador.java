006e2f3792b0f15fe806e5d3db581437ab3b9bc8
Filipe Fuentes Giroleti <101720928+FuentesFilipe@users.noreply.github.com>
2023-03-24 15:33:04 -0300

    up: updated the way Smilador.java reads the file
    Simulador.java previously iterated over every line of the file using a while loop (while line != null), it now iterates using the Files.readAllLines() method.

--- Simulador.java
+++ Simulador.java
@@ -1,45 +1,44 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Simulador {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("src/rodadas.txt"));
-        String line = reader.readLine();
+        // Reading the file with readAllLines()
+        List<String> lines = Files.readAllLines(Paths.get("src/rodadas.txt"));
 
-        // criar variavel pra armazenar numero de rodadas e a coleção de macacos
+
         int numRodadas = 0;
         ArrayList<Macaquinho> macacos = new ArrayList<>();
-
-        while (line != null) { // START DO LOOP
+        for (String line : lines) { // INICIO DO ARQUIVO
             String[] words = line.split(" ");
+            if (line.startsWith("Macaco")) {
+                // pega o Id do macaco da linha
+                int id = Integer.parseInt(words[1]);
 
-             if (line.startsWith("Macaco")) {
-                 // pega o Id do macaco da linha
-                 int id = Integer.parseInt(words[1]);
+                // pega os Id dos macacos que irão receber cocos em caso de par ou impar nas pedrinhas
+                int par = Integer.parseInt(words[4]);
+                int impar = Integer.parseInt(words[7]);
 
-                 // pega os Id dos macacos que irão receber cocos em caso de par ou impar nas pedrinhas
-                 int par = Integer.parseInt(words[4]);
-                 int impar = Integer.parseInt(words[7]);
+                // inicializa a lista de cocos de cada macaco
+                ArrayList<Integer> cocos = new ArrayList<>();
 
-                 // inicializa a lista de cocos de cada macaco
-                 ArrayList<Integer> cocos = new ArrayList<>();
+                // pega os valores das pedrinhas em cada coco
+                for (int i = 11; i < words.length; i++) {
+                    cocos.add(Integer.parseInt(words[i]));
+                }
 
-                 // pega os valores das pedrinhas em cada coco
-                 for (int i = 11; i < words.length; i++) {
-                     cocos.add(Integer.parseInt(words[i]));
-                 }
-
-                 // inicializa o macaco da linha com as informações obtidas
-                 Macaquinho macaco = new Macaquinho(id, par, impar, cocos);
-                 macacos.add(macaco);
-             } else {
-                 // pega o numero de rodadas da primeira linha
-                 numRodadas = Integer.parseInt(words[1]);
-             }
-            line = reader.readLine();
-        } // FINAL DO LOOP
-        reader.close(); // FINAL LEITURA ARQUIVO
+                // inicializa o macaco da linha com as informações obtidas
+                Macaquinho macaco = new Macaquinho(id, par, impar, cocos);
+                macacos.add(macaco);
+            } else {
+                // pega o numero de rodadas da primeira linha
+                numRodadas = Integer.parseInt(words[1]);
+            }
+        }// FINAL DO ARQUIVO
         int numMacacos = macacos.size();
 
         // INICIO JOGO
@@ -54,6 +53,7 @@ public class Simulador {
             }
         } // FINAL DO JOGO
 
+        System.out.printf("Resultado dos Macacos após %d rodadas: \n", numRodadas);
         for (Macaquinho macaco:
              macacos) {
             System.out.println(macaco.getCocos());

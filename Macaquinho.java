f82b314c8536606bbb612d7af500434494a0b0ae
Filipe Fuentes Giroleti <101720928+FuentesFilipe@users.noreply.github.com>
2023-03-24 15:00:35 -0300

    up: updated the enviarCoco method
    enviarCoco would previously only send a single coconut but not atually remove it from the coconut list.

--- Macaquinho.java
+++ Macaquinho.java
@@ -1,41 +1,44 @@
-//import java.util.List;
-//
-//public class Macaquinho {
-//    private int id;
-//    private int par;
-//    private int impar;
-//    private List<Coco> cocos;
-//
-//    public Macaquinho(int id, int par, int impar, List<Coco> cocos) {
-//        this.id = id;
-//        this.par = par;
-//        this.impar = impar;
-//        this.cocos = cocos;
-//    }
-//
-//    public void enviarCoco(Coco coco, List<Macaquinho> macaquinhos) {
-//        int destino;
-//        if (coco.getNumPedrinhas() % 2 == 0) {
-//            destino = par;
-//        } else {
-//            destino = impar;
-//        }
-//        macaquinhos.get(destino).receberCoco(coco);
-//    }
-//
-//    public void receberCoco(Coco coco) {
-//        cocos.add(coco);
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public List<Coco> getCocos() {
-//        return cocos;
-//    }
-//
-//    public int getNumCocos() {
-//        return cocos.size();
-//    }
-//}
+import java.util.List;
+
+public class Macaquinho {
+    private int id;
+    private int par;
+    private int impar;
+    private List<Integer> cocos;
+
+    public Macaquinho(int id, int par, int impar, List<Integer> cocos) {
+        this.id = id;
+        this.par = par;
+        this.impar = impar;
+        this.cocos = cocos;
+    }
+
+    public void enviarCoco(int coco, List<Macaquinho> macaquinhos) {
+
+        this.cocos.remove(cocos.indexOf(coco));
+
+        int destino;
+        if (coco % 2 == 0) {
+            destino = par;
+        } else {
+            destino = impar;
+        }
+        macaquinhos.get(destino).receberCoco(coco);
+    }
+
+    public void receberCoco(int coco) {
+        cocos.add(coco);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Integer> getCocos() {
+        return cocos;
+    }
+
+    public Integer getNumCocos() {
+        return this.getCocos().size();
+    }
+}
